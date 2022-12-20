3af974939d0c68ac30e4cf8e9c20d38ac73445c0
Michael Hu <mercihu0318@gmail.com>
2022-12-21 02:06:56 +0800

    gsdownload

--- playground/gsdownload.py
+++ playground/gsdownload.py
@@ -6,7 +6,9 @@ import os, sys
 print(os.path.abspath(__file__))
 
 root = '/home/Melody-Diffusion/data/splits/split-0/autotagging-train.tsv'
+# root = 'data/splits/split-0/autotagging-train.tsv'
 prefix = 'gs://asm-ai-data/Michael/Database/MTG_Audio/'
+prefix_local = '/home/MTG_Audio/'
 
 
 def get_files_path(root, prefix):
@@ -15,7 +17,7 @@ def get_files_path(root, prefix):
         reader = csv.reader(f, delimiter='\t')
         next(reader, None)
         for row in reader:
-            l.append(''.join([prefix, row[3]]))
+            l.append(row[3])
     return l
 
 
@@ -23,7 +25,9 @@ if __name__ == "__main__":
     from tqdm import tqdm
     files = get_files_path(root, prefix)
     client = storage.Client(project="ai-innovation-370705")
-    Path('/home/MTG_Audio').mkdir(parents=True, exist_ok=True)
-    for file in tqdm(files):
-        with open(''.join(['/home',file.split('Database')[-1]])) as file_obj:
-            client.download_blob_to_file(file, file_obj)
+    Path(prefix_local).mkdir(parents=True, exist_ok=True)
+    for file in tqdm(files):        
+        if not (Path(prefix_local)/file.split('/')[0]).exists():
+            (Path(prefix_local)/file.split('/')[0]).mkdir(parents=True, exist_ok=True)
+        with open(Path(prefix_local)/file) as file_obj:
+            client.download_blob_to_file('gs://asm-ai-data/Michael/Database/MTG_Audio/'+file, file_obj)
