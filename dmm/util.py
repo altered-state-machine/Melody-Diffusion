b104ba773eb3c01466a9dd9ee234fb0b3851bcb5
Michael Hu <mercihu0318@gmail.com>
2022-12-21 22:49:38 +0800

    cond ver.0.1.5

--- dmm/util.py
+++ dmm/util.py
@@ -1,5 +1,5 @@
 import importlib
-import librosa
+# import librosa
 
 import torch
 import numpy as np
@@ -207,23 +207,23 @@ def parallel_data_prefetch(
         return gather_res
 
 
-def image_to_audio(image: np.ndarray) -> np.ndarray:
-    """Converts spectrogram to audio.
+# def image_to_audio(image: np.ndarray) -> np.ndarray:
+#     """Converts spectrogram to audio.
 
-    Args:
-        image (np.ndarray): x_res x y_res grayscale image
+#     Args:
+#         image (np.ndarray): x_res x y_res grayscale image
 
-    Returns:
-        audio (np.ndarray): raw audio
-    """
-    # bytedata = np.frombuffer(image.tobytes(), dtype="uint8").reshape((image.height, image.width))
-    # log_S = bytedata.astype("float") * 80 / 255 - 80 # top_db: 80
-    log_S = image.squeeze() * 40
-    S = librosa.db_to_power(log_S)
-    audio = librosa.feature.inverse.mel_to_audio(
-        S, sr=22050, n_fft=2048, hop_length=512, n_iter=32
-    )
-    return audio
+#     Returns:
+#         audio (np.ndarray): raw audio
+#     """
+#     # bytedata = np.frombuffer(image.tobytes(), dtype="uint8").reshape((image.height, image.width))
+#     # log_S = bytedata.astype("float") * 80 / 255 - 80 # top_db: 80
+#     log_S = image.squeeze() * 40
+#     S = librosa.db_to_power(log_S)
+#     audio = librosa.feature.inverse.mel_to_audio(
+#         S, sr=22050, n_fft=2048, hop_length=512, n_iter=32
+#     )
+#     return audio
 
 
 
